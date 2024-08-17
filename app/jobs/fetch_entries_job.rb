61c50ed2a2ff67c41f85b59983291a317158946a
shota-nagato <shota.nagato@gmail.com>
2024-08-17 20:30:33 +0900

    rssの画像を取得できるようにする

--- app/jobs/fetch_entries_job.rb
+++ app/jobs/fetch_entries_job.rb
@@ -7,9 +7,9 @@ class FetchEntriesJob
       response = Faraday.get(feed.url)
       rss = Feedjira.parse(response.body)
       rss.entries.each do |entry|
-        # 前日に投稿されたエントリーを保存する
-        if entry.published.to_date == Date.yesterday
-          feed.entries.find_or_create_by(title: entry.title, url: entry.url, published_at: entry.published)
+        e = feed.entries.find_or_create_by(title: entry.title, url: entry.url, published_at: entry.published)
+        if entry.image.present?
+          e.update!(image_url: entry.image)
         end
       end
     end
