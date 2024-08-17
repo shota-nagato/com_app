43f97fc667597062c9e58b51565a474a4bc20ce8
shota-nagato <shota.nagato@gmail.com>
2024-08-17 20:32:40 +0900

    変数名を変更

--- app/jobs/fetch_entries_job.rb
+++ app/jobs/fetch_entries_job.rb
@@ -6,10 +6,10 @@ class FetchEntriesJob
     Feed.all.each do |feed|
       response = Faraday.get(feed.url)
       rss = Feedjira.parse(response.body)
-      rss.entries.each do |entry|
-        e = feed.entries.find_or_create_by(title: entry.title, url: entry.url, published_at: entry.published)
-        if entry.image.present?
-          e.update!(image_url: entry.image)
+      rss.entries.each do |rss_entry|
+        entry = feed.entries.find_or_create_by(title: rss_entry.title, url: rss_entry.url, published_at: rss_entry.published)
+        if rss_entry.image.present?
+          entry.update!(image_url: rss_entry.image)
         end
       end
     end
