02b91e74a2c870bfad6a1a69ad4e1cce5bfd872c
shota-nagato <shota.nagato@gmail.com>
2024-08-22 00:33:37 +0900

    質問テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_17_111833) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_21_145754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_111833) do
     t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", default: "", null: false
+    t.text "content", default: "", null: false
+    t.datetime "solved_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +96,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_111833) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "entries", "feeds"
   add_foreign_key "feeds", "users"
+  add_foreign_key "questions", "users"
 end
