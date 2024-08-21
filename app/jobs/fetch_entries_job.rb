413d61261b908f613b09af0f24aa1b2fb89b8a7a
shota-nagato <shota.nagato@gmail.com>
2024-08-21 21:58:55 +0900

    feedを更新して記事を取得できる

--- app/jobs/fetch_entries_job.rb
+++ app/jobs/fetch_entries_job.rb
@@ -4,8 +4,8 @@ class FetchEntriesJob
 
   def perform(feed_id)
     feed = Feed.find(feed_id)
-    response = Faraday.get(feed.url)
-    rss = Feedjira.parse(response.body)
+    rss = fetch_rss(feed)
+
     rss.entries.each do |rss_entry|
       entry = feed.entries.find_or_create_by(title: rss_entry.title, url: rss_entry.url, published_at: rss_entry.published)
       if rss_entry.image.present?
@@ -14,5 +14,12 @@ class FetchEntriesJob
     end
 
     feed.update!(updated_at: Time.current)
+  end
+
+  private
+
+  def fetch_rss(feed)
+    response = Faraday.get(feed.url)
+    rss = Feedjira.parse(response.body)
   end
 end
