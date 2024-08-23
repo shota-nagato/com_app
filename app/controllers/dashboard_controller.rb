de9ae6216afa701c409f026e1e17a4d037de9d34
shota-nagato <shota.nagato@gmail.com>
2024-08-23 19:58:47 +0900

    一覧のUI

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def show
     @entries = Entry.preload(feed: :user).order(published_at: :desc).limit(5)
+    @questions = Question.preload(:user).order(created_at: :desc).limit(5)
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @questions = Question.order(created_at: :desc)
+    @questions = Question.preload(:user).order(created_at: :desc)
   end
 
   def new
