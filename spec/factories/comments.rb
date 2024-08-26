375e0dece2d6a7c6c6dd7990fd0f7a095ba4d63b
shota-nagato <shota.nagato@gmail.com>
2024-08-27 00:33:42 +0900

    モデルスペック追加

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    
+    content { "コメント" }
+    association :user
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user:) }
+
+  before do
+    create(:comment, user:, commentable: question)
+  end
+
+  it "質問を削除後、紐付くコメントも削除される" do
+    expect do
+      question.destroy!
+    end.to change(Comment, :count).by(-1)
+  end
 end
