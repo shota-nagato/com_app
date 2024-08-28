4de40eaf0203464b917f25dff5d656bdd69a4e87
shota-nagato <shota.nagato@gmail.com>
2024-08-28 23:26:10 +0900

    コメント時にユーザーに通知する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,21 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true, touch: true
   belongs_to :user
+  has_noticed_notifications model_name: "Noticed::Event"
+  has_many :notification_mentions, as: :record, dependent: :destroy, class_name: "Noticed::Event"
 
   validates :content, presence: true
+
+  after_create_commit :notify_recipient
+  before_destroy :cleanup_notifications
+
+  private
+
+  def notify_recipient
+    CommentNotifier.with(record: self, commentable:).deliver_later(commentable.user)
+  end
+
+  def cleanup_notifications
+    commentable.notifications.where(id: commentable.id).destroy_all
+  end
 end
--- app/notifiers/comment_notifier.rb
+++ app/notifiers/comment_notifier.rb
@@ -5,10 +5,10 @@
 class CommentNotifier < ApplicationNotifier
   # Add your delivery methods
   #
-  deliver_by :email do |config|
-    config.mailer = "UserMailer"
-    config.method = "new_comment"
-  end
+  # deliver_by :email do |config|
+  #   config.mailer = "UserMailer"
+  #   config.method = "new_comment"
+  # end
   #
   # bulk_deliver_by :slack do |config|
   #   config.url = -> { Rails.application.credentials.slack_webhook_url }
@@ -21,4 +21,19 @@ class CommentNotifier < ApplicationNotifier
   # Add required params
   #
   # required_param :message
+
+  def message
+    @commentable = record.commentable
+    @user = record.user
+    "#{@user.name}が#{@commentable.model_name.human}にコメントしました。"
+  end
+
+  def url
+    case record.commentable.class.name
+    when "Question"
+      question_path(record.commentable)
+    else
+      ""
+    end
+  end
 end
