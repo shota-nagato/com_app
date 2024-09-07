c1d7ee927b00c9f34e63d797ba672aeb1fca0ee6
shota-nagato <shota.nagato@gmail.com>
2024-09-07 14:42:41 +0900

    管理画面を作成

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,9 @@
+class Admin::ApplicationController < ActionController::Base
+  before_action :authenticate_admin
+
+  layout "admin/application"
+
+  def authenticate_admin
+    redirect_to "/", alert: "Not authorized." unless user_signed_in? && current_user.admin?
+  end
+end
