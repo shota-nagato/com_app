c98ef5dfae2462a867c50217647949f6243046ab
shota-nagato <shota.nagato@gmail.com>
2024-09-11 21:25:30 +0900

    不要な設定を削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,12 +67,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     WebMock.disable_net_connect!(
       allow_localhost: true,
-      allow: [
-        "chromedriver.storage.googleapis.com",
-        "rails-app",
-        "selenium",
-        "https://toyokeizai.net/list/feed/rss"
-      ]
     )
   end
 end
