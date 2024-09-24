509ea283ee7154d10f2beb1593e4f81ef8834f3e
shota-nagato <shota.nagato@gmail.com>
2024-09-24 20:11:40 +0900

    rate limit追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
 
   authenticated :user, ->(u) { u.admin? } do
